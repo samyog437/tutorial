917d51c20dd4dd7fa6bc649572fa557bc446b924
samyog437 <fallendemon174@gmail.com>
2021-09-20 21:35:08 +0545

    Initial commit

---
+++ sprite example.py
@@ -0,0 +1,64 @@
+import pygame
+import random
+import os
+
+WIDTH = 800
+HEIGHT = 600
+FPS = 30
+
+# define colours
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+
+# set up assets folders
+game_folder = os.path.dirname(__file__)
+img_folder = os.path.join(game_folder, "asset")
+
+
+class Player(pygame.sprite.Sprite):
+    # sprite for the player
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load(os.path.join(img_folder, "Base pack/Player/p1_jump.png")).convert()
+        self.rect = self.image.get_rect()
+        self.rect.center = (WIDTH / 2, HEIGHT / 2)
+
+    def update(self):
+        self.rect.x += 5
+        if self.rect.left > WIDTH:
+            self.rect.right = 0
+
+
+# initialize pygame and create window
+pygame.init()
+pygame.mixer.init()
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("My Game")
+clock = pygame.time.Clock()
+
+all_sprites = pygame.sprite.Group()
+player = Player()
+all_sprites.add(player)
+# Game loop
+running = True
+while running:
+    # keep loop running at the right speed
+    clock.tick(FPS)
+    # Process input(events)
+    for event in pygame.event.get():
+        # check for closing window
+        if event.type == pygame.QUIT:
+            running = False
+    # Update
+    all_sprites.update()
+    # Draw/render
+    screen.fill(BLACK)
+    all_sprites.draw(screen)
+
+    # *after* drawing everything, flip the display
+    pygame.display.flip()
+
+pygame.quit()
